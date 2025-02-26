ee2bb78308ef4eb5f573451b6465fd160aa9b845
Alexander Shiyan <eagle.alexander923@gmail.com>
2025-02-26 12:59:16 +0300

    milas: Modernize post-image script

--- board/milas/post-image.sh
+++ board/milas/post-image.sh
@@ -4,6 +4,7 @@
 dd if=/dev/zero of=$BINARIES_DIR/barebox.env bs=1M count=1
 
 # Generate image
-support/scripts/genimage.sh -c board/milas/genimage.cfg
+BOARD_DIR="$(dirname $0)"
+support/scripts/genimage.sh -c $BOARD_DIR/genimage.cfg
 
 exit 0
