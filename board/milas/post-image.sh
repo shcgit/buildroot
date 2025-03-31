ad0f6dba19b3c57dad4d93f370e638f5b8c609f3
Alexander Shiyan <eagle.alexander923@gmail.com>
2025-03-31 10:26:58 +0300

    sama5d2: Reduce environment file size to 256K

--- board/milas/post-image.sh
+++ board/milas/post-image.sh
@@ -1,7 +1,7 @@
 #!/bin/bash
 
 # Generate empty environment
-dd if=/dev/zero of=$BINARIES_DIR/barebox.env bs=1M count=1
+dd if=/dev/zero of=$BINARIES_DIR/barebox.env bs=256K count=1
 
 # Generate image
 BOARD_DIR="$(dirname $0)"
